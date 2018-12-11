b43fce1cda61076d62cf5239fd8a85c20f089b30
Jeremy Yen <jeremy5189@gmail.com>
2018-12-11 17:37:29 +0800

    Remove config, add example config

---
+++ src/config.example.js
@@ -0,0 +1,8 @@
+export default {
+  baseURL: 'http://localhost:8003',
+  baseURLVTime: 'http://localhost:8004',
+  teamPath: '/team',
+  vtimePath: '/vtime',
+  fetchInterval: 1000 * 5,
+  fetchVTimeInterval: 1000,
+};
